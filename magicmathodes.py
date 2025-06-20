e7226f4d2f345fa360cac25957be971f5e702fd1
Shashank SH <shashankssh07@gmail.com>
2025-06-20 15:49:05 +0530

    Initial commit

---
+++ magicmathodes.py
@@ -0,0 +1,11 @@
+#basic magic mathodes
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"{self.name}, {self.age} years old"
+
+person = Person("Alice", 30)
+print(person)
---
+++ py.py
@@ -0,0 +1,10 @@
+class dog:
+    def char(name,age):
+        print("Name:", name)
+        print("Age:", age)
+
+print("Enter the name of the dog:")
+name = input()
+print("Enter the age of the dog:")
+age = input()
+dog.char(name, age)
